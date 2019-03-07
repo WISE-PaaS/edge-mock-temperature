e0bd39ba1ff62d966b448a866ba653b04a85bf86
Dave.Lee <Dave.Lee@advantech.com>
2019-03-08 03:25:05 +0800

    direct uri

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const mqtt = require('mqtt')
 
 // -- Get env variables for rabbitmq service
-const vcapServices = JSON.parse(process.env.VCAP_SERVICES);
-const mqttUri = vcapServices['p-rabbitmq'][0].credentials.protocols.mqtt.uri
+// const vcapServices = JSON.parse(process.env.VCAP_SERVICES);
+// const mqttUri = vcapServices['p-rabbitmq'][0].credentials.protocols.mqtt.uri
+const mqttUri = 'mqtt://eb869bc9-31f7-46dd-928b-4c6e08a65302%3A34341485-7f96-4eca-a93b-397a6ec23fbe:AxFiXoqYCZwJqRooDYFUWrK2D@wise-msghub.eastasia.cloudapp.azure.com:1883'
 
 // const connectOpts = {
 //   host: ,
