58b129c476b41930aa52515305fc36777a3cfb41
Dave <isho.lee@gmail.com>
2019-03-19 13:41:14 +0800

    use vcap

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const mqtt = require('mqtt')
 // -- Get env variables for rabbitmq service
 const vcapServices = JSON.parse(process.env.VCAP_SERVICES);
 const mqttUri = vcapServices['p-rabbitmq'][0].credentials.protocols.mqtt.uri
-
+// externalHost: wise-msghub.eastasia.cloudapp.azure.com
+// const mqttUri = 'mqtt://eb869bc9-31f7-46dd-928b-4c6e08a65302%3Aad063d1e-2ae5-4566-94c3-1a513b19ed73:s6jX0eEiuvpclN3C5EWv9qIhG@wise-msghub.eastasia.cloudapp.azure.com:1883';
 // const connectOpts = {
 //   host: ,
 //   port: 1883,
@@ -24,7 +25,7 @@ client.on('connect', (connack) => {
 function publishMockTemp() {
   const temp = Math.floor((Math.random() * 7) + 22);
 
-  client.publish('livingroom/temperature', temp.toString(), { qos: 2 }, (err, packet) => {
-    if (!err) console.log('Data sent to livingroom/temperature -- ' + temp);
+  client.publish('building_hq/b100/temperature', temp.toString(), { qos: 2 }, (err, packet) => {
+    if (!err) console.log('Data sent to building_hq/b100/temperature -- ' + temp);
   });
 }
