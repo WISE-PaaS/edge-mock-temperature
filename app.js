2e0bde3c756c23c4cd652cb7035001a2142d35e8
Yihsiu <isho.lee@gmail.com>
2019-07-24 13:48:18 +0800

    adding exertalHost reference for wise-paas.io

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const mqtt = require('mqtt')
 
 // -- Get env variables for rabbitmq service
 const vcapServices = JSON.parse(process.env.VCAP_SERVICES);
-const mqttUri = vcapServices['p-rabbitmq'][0].credentials.protocols.mqtt.uri
-// externalHost: wise-msghub.eastasia.cloudapp.azure.com
+const mqttUri = vcapServices['p-rabbitmq-innoworks'][0].credentials.protocols.mqtt.uri
+// externalHost(wise-paas.com): wise-msghub.eastasia.cloudapp.azure.com
+// externalHost(wise-paas.io): 40.81.26.31
 // const mqttUri = 'mqtt://eb869bc9-31f7-46dd-928b-4c6e08a65302%3Aad063d1e-2ae5-4566-94c3-1a513b19ed73:s6jX0eEiuvpclN3C5EWv9qIhG@wise-msghub.eastasia.cloudapp.azure.com:1883';
 // const connectOpts = {
 //   host: ,
@@ -25,7 +26,7 @@ client.on('connect', (connack) => {
 function publishMockTemp() {
   const temp = Math.floor((Math.random() * 7) + 22);
 
-  client.publish('building_hq/b100/temperature', temp.toString(), { qos: 2 }, (err, packet) => {
-    if (!err) console.log('Data sent to building_hq/b100/temperature -- ' + temp);
+  client.publish('livingroom/temperature', temp.toString(), { qos: 2 }, (err, packet) => {
+    if (!err) console.log('Data sent to livingroom/temperature -- ' + temp);
   });
 }
